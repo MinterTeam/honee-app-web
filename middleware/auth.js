153654049514248259360842359815f66542191c
shrpne <shrpne@gmail.com>
2021-09-06 15:28:44 +0300

    update auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+import {DASHBOARD_URL} from '~/assets/variables.js';
+
 export default function({store, route, redirect, error}) {
     if (process.server) {
         return;
@@ -7,15 +9,16 @@ export default function({store, route, redirect, error}) {
     console.log('-- path', route.path);
 
     const urlRequiresNonAuth = /^\/auth(\/|$)/.test(route.path);
+    const urlRequiresAuth = /^\/dashboard(\/|$)/.test(route.path);
 
-    if (!store.getters.isAuthorized && !urlRequiresNonAuth) {
+    if (!store.getters.isAuthorized && urlRequiresAuth) {
         console.log('-- restricted: redirect to auth');
         store.commit('SET_AUTH_REDIRECT_PATH', route.fullPath);
         return redirect('/auth');
     }
     if (store.getters.isAuthorized && urlRequiresNonAuth) {
         console.log('-- restricted: redirect to index');
-        return redirect('/');
+        return redirect(DASHBOARD_URL);
     }
 
     console.log('-- not restricted');
--- pages/index.vue
+++ pages/index.vue
@@ -17,8 +17,8 @@ export default {
             <div class="container">
                 <div class="hello">
                     <div class="hello-content">
-                        <div class="logo"><img src="/img/logo-honee.svg" alt="" /></div>
-                        <h1>The simplest  crypto wallet that helps you earn</h1>
+                        <div class="logo"><img src="/img/logo-honee.svg" alt="" width="122" height="24"/></div>
+                        <h1>The simplest crypto wallet that helps you earn</h1>
                         <p class="subtitle-h1">Honee wallet is focused on enabling people to earn crypto.</p>
                         <!-- <a class="btn" href="#">Start Earning</a> -->
                         <h3>Coming this September</h3>
@@ -33,32 +33,32 @@ export default {
                 <p class="subtitle-h2">Find out why Honee is <b><i>the</i></b> crypto wallet</p>
                 <div class="features-items">
                     <div class="features-item">
-                        <img src="/img/landing/f1.svg" alt="">
+                        <img src="/img/landing/f1.svg" alt="" width="64" height="64">
                         <h3>Great to Earn With</h3>
                         <p>Crypto and decentralized finance are complicated, but we did all the hard work for you. Enjoy our one-click solutions to apply the best earning tools to your balance.</p>
                     </div>
                     <div class="features-item">
-                        <img src="/img/landing/f2.svg" alt="">
+                        <img src="/img/landing/f2.svg" alt="" width="64" height="64">
                         <h3>Simple to Use</h3>
                         <p>Aside from helping you earn, our second-biggest goal is to provide a smooth user experience. Honee is simple and user-friendly at every step of the way.</p>
                     </div>
                     <div class="features-item">
-                        <img src="/img/landing/f3.svg" alt="">
+                        <img src="/img/landing/f3.svg" alt="" width="64" height="64">
                         <h3>Best to Trade At</h3>
                         <p>Buy, sell, and swap your Bitcoin, Ether, USDT, and other popular cryptos instantly & at the lowest fees in the industry.</p>
                     </div>
                     <div class="features-item">
-                        <img src="/img/landing/f4.svg" alt="">
+                        <img src="/img/landing/f4.svg" alt="" width="64" height="64">
                         <h3>Private & Secure</h3>
                         <p>You are the only person who can access your wallet. We don’t collect any personal data.</p>
                     </div>
                     <div class="features-item">
-                        <img src="/img/landing/f5.svg" alt="">
+                        <img src="/img/landing/f5.svg" alt="" width="64" height="64">
                         <h3>Fully Decentralized</h3>
                         <p>It means that only you own your money, not an exchange or a wallet. No one can stop or limit your actions.</p>
                     </div>
                     <div class="features-item">
-                        <img src="/img/landing/f6.svg" alt="">
+                        <img src="/img/landing/f6.svg" alt="" width="64" height="64">
                         <h3>Rewarding</h3>
                         <p>Honee has rich programs to reward users for different actions. It’s the best way to win crypto or get it for free.</p>
                     </div>
@@ -72,7 +72,7 @@ export default {
                 <div class="steps-items">
                     <div class="steps-item">
                         <div class="steps-icon">
-                            <img src="/img/landing/step1.svg" alt="">
+                            <img src="/img/landing/step1.svg" alt="" width="64" height="64">
                             <span class="badge">1</span>
                         </div>
                         <h3>Create a new wallet</h3>
@@ -80,7 +80,7 @@ export default {
                     <img src="/img/landing/arrow-right.svg" class="arrow" alt="">
                     <div class="steps-item">
                         <div class="steps-icon">
-                            <img src="/img/landing/step2.svg" alt="">
+                            <img src="/img/landing/step2.svg" alt="" width="64" height="64">
                             <span class="badge">2</span>
                         </div>
                         <h3>Top it up</h3>
@@ -93,12 +93,22 @@ export default {
             <div class="container">
                 <h2>Follow us</h2>
                 <div class="socials">
-                    <a href="https://twitter.com/HoneeApp" target="_blank"><img src="/img/landing/twitter.svg" alt=""></a>
-                    <a href="https://t.me/HoneeApp" target="_blank"><img src="/img/landing/telegram.svg" alt=""></a>
-                    <a href="https://www.youtube.com/channel/UChqsLhknZKhKfNxmMT7yytQ" target="_blank"><img src="/img/landing/youtube.svg" alt=""></a>
-                    <a href="https://www.facebook.com/honeeapp" target="_blank"><img src="/img/landing/facebook.svg" alt=""></a>
-                    <a href="https://medium.com/@honeeapp" target="_blank"><img src="/img/landing/medium.svg" alt=""></a>
-                    <!--<a href="#" target="_blank"><img src="/img/landing/instagram.svg" alt=""></a>-->
+                    <a href="https://twitter.com/HoneeApp" target="_blank">
+                        <img src="/img/landing/twitter.svg" alt="" width="64" height="64">
+                    </a>
+                    <a href="https://t.me/HoneeApp" target="_blank">
+                        <img src="/img/landing/telegram.svg" alt="" width="64" height="64">
+                    </a>
+                    <a href="https://www.youtube.com/channel/UChqsLhknZKhKfNxmMT7yytQ" target="_blank">
+                        <img src="/img/landing/youtube.svg" alt="" width="64" height="64">
+                    </a>
+                    <a href="https://www.facebook.com/honeeapp" target="_blank">
+                        <img src="/img/landing/facebook.svg" alt="" width="64" height="64">
+                    </a>
+                    <a href="https://medium.com/@honeeapp" target="_blank">
+                        <img src="/img/landing/medium.svg" alt="" width="64" height="64">
+                    </a>
+                    <!--<a href="#" target="_blank"><img src="/img/landing/instagram.svg" alt="" width="64" height="64"></a>-->
                 </div>
             </div>
         </section>
@@ -110,7 +120,7 @@ export default {
                     <p>Ullamcorper augue ullamcorper eget adipiscing mi neque. At elementum, porttitor adipiscing mauris ullamcorper. Tellus fames condimentum sem felis. Pretium pharetra risus nibh lectus sed et ultrices. Donec pulvinar varius tincidunt adipiscing leo lobortis morbi.</p>
                     <p>Consequat tortor nisl velit felis posuere porttitor lobortis hendrerit porttitor. Fusce leo augue odio scelerisque.</p>
                 </div>-->
-                <div class="logo"><img src="/img/logo-honee.svg" alt="" /></div>
+                <div class="logo"><img src="/img/logo-honee.svg" alt="" width="82" height="16"/></div>
                 <div class="footer-copyright-container">
                     <div class="footer-copyright">
                         Copyright © 2021 Grom R&D Limited.<br>
