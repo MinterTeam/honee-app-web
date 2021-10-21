3d8a313d627d975dc712b6c71fc5067d01b504c4
Just call me Dan <68339882+danzybee@users.noreply.github.com>
2021-10-21 19:24:32 +0300

    Update FieldCombinedBaseDropdown.vue

--- components/base/FieldCombinedBaseDropdown.vue
+++ components/base/FieldCombinedBaseDropdown.vue
@@ -39,7 +39,7 @@ export default {
         },
         inputPlaceholder: {
             type: String,
-            default: this.$td('Search…', 'common.search'),
+            const default = getDefault(this.$td('Search…', 'common.search')),
         },
     },
     data() {
