a4aee1636668576c8a6ad7012f17850ba43531d0
Just call me Dan <68339882+danzybee@users.noreply.github.com>
2021-10-22 21:20:16 +0300

    Update TxStakeDelegateForm.vue

--- components/TxStakeDelegateForm.vue
+++ components/TxStakeDelegateForm.vue
@@ -145,7 +145,7 @@ export default {
         <template v-slot:confirm-modal-header>
             <h1 class="panel__header-title">
 <!--                <img class="panel__header-title-icon" :src="`${BASE_URL_PREFIX}/img/icon-delegate.svg`" alt="" role="presentation" width="40" height="40">-->
-                {{ $td('Delegate', 'delegation.delegate-title') }}
+                {{ $td('Delegate', 'action.title-delegate') }}
             </h1>
         </template>
 
