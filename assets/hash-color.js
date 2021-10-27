6f88ef349aac04bcf36c5f4fc6c5a07cab79275a
shrpne <shrpne@gmail.com>
2021-10-27 21:02:59 +0300

    tune colors

--- assets/hash-color.js
+++ assets/hash-color.js
@@ -8,12 +8,42 @@ import {LCH_to_sRGB_string} from '~/assets/csswg-color4.js';
  */
 export default function hashColor(str) {
     const hash = Math.abs(murmurhash.murmur2(str));
-    // lightness 40-50
-    const L = 40 + hash % 10;
+    const BASE_L = 40;
+    // lightness 40-60
+    let L = BASE_L + hash % 20;
     // chroma 75-85
-    const C = 75 + hash % 10;
+    let C = 65 + hash % 20;
     // hue
-    const H = hash % 359;
+    let H = hash % 359;
+
+    const DIRT_LOW = 70;
+    const DIRT_HIGH = 95;
+    // shift to more red to discard dirt colors
+    if (H > DIRT_LOW && H < DIRT_HIGH) {
+        H = H - (DIRT_HIGH - DIRT_LOW);
+    }
+
+    const ORANGE_LOW = 55;
+    // make orange colors lighter to not be dirt
+    if (H > ORANGE_LOW && H <= DIRT_HIGH) {
+        L = BASE_L + 20 + hash % 5;
+        C = C + 10;
+    }
+
+    const BLUE_LOW = 240;
+    const BLUE_HIGH = 300;
+    // reduce white to make blue colors juicy
+    if (H > BLUE_LOW && H < BLUE_HIGH) {
+        L = BASE_L - 5 + hash % 10;
+        C = C + 10;
+    }
+
+    const VIOLET_HIGH = 330;
+    // make violet not to light
+    if (H >= BLUE_HIGH && H < VIOLET_HIGH) {
+        L = BASE_L + hash % 10;
+        C = C + 10;
+    }
     console.log({L, C, H}, LCH_to_sRGB_string(L, C, H), str);
     return LCH_to_sRGB_string(L, C, H);
 }
