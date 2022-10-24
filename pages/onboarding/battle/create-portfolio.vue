941790b0e2d9879281de64dabc54706fd660685c
shrpne <shrpne@gmail.com>
2022-10-24 18:08:50 +0300

    feat(battle): /onboarding/battle - check if portfolio already created

--- pages/onboarding/battle/create-portfolio.vue
+++ pages/onboarding/battle/create-portfolio.vue
@@ -18,10 +18,16 @@ export default {
 
 <template>
     <div class="u-section u-container u-container--small">
-        <div class="card card--invert">
+        <div class="card card--invert card--transparent">
             <div class="card__content card__content--medium">
                 <h1 class="card__action-title-value">{{ $td('Create portfolio', 'portfolio.manage-create-title') }}</h1>
-<!--                <p class="card__action-description u-mt-05">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa pellentesque donec in mus mi massa fusce netus.</p>-->
+                <p class="card__action-description u-mt-05">
+                    {{ $td('Choose from 2 to 10 tokens and specify the allocation percentage.', 'battle.create-description-1') }}
+                </p>
+                <p class="card__action-description">
+                    <strong>{{ $td('Remember:', 'battle.create-remember') }}</strong>
+                    {{ $td('you can create only one portfolio and edit it once a day.', 'battle.create-description21') }}
+                </p>
             </div>
 
             <PortfolioManageForm/>
--- pages/onboarding/battle/index.vue
+++ pages/onboarding/battle/index.vue
@@ -1,16 +1,30 @@
 <script>
+import {getPortfolioList} from '~/api/portfolio.js';
+import BaseLoader from '~/components/base/BaseLoader.vue';
 import TelegramAuth from '~/components/TelegramAuth.vue';
 
 export default {
     layout: 'splash',
     components: {
+        BaseLoader,
         TelegramAuth,
+    },
+    fetch() {
+        return getPortfolioList({owner: this.$store.getters.address})
+            .then((result) => {
+                this.portfolio = result.list[0];
+            });
     },
     head() {
         return {
             htmlAttrs: {
                 class: 'splash-layout splash-layout--battle',
             },
+        };
+    },
+    data() {
+        return {
+            portfolio: undefined,
         };
     },
     methods: {
@@ -23,18 +37,36 @@ export default {
         <div class="card card__content card--light-grey u-text-center">
             <img class="u-mb-05" src="/img/battle-begin.png" srcset="/img/battle-begin@2x.png 2x" alt="" role="presentation">
             <h1 class="u-h3 u-mb-05">{{ $td('Let the Battle Begin!', 'battle.begin-title') }}</h1>
-            <p>{{ $td('You’ve registered for the tournament. Now you need to login via Telegram and create your portfolio.', 'battle.begin-description') }}</p>
 
-            <TelegramAuth class="u-mt-10 u-mb-10"/>
+            <div v-if="$fetchState.pending" class="u-text-center u-mt-10">
+                <BaseLoader class="" :is-loading="true"/>
+            </div>
 
-            <component
-                :is="$store.getters['telegram/isAuthorized'] ? 'nuxt-link' : 'div'"
-                class="button button--main button--full"
-                :class="{'is-disabled': !$store.getters['telegram/isAuthorized']}"
-                :to="$i18nGetPreferredPath('/onboarding/battle/create-portfolio')"
-            >
-                {{ $td('Create portfolio', 'portfolio.create-new-link') }}
-            </component>
+            <template v-else-if="!portfolio">
+                <p>{{ $td('You’ve registered for the tournament. Now you need to login via Telegram and create your portfolio.', 'battle.begin-description') }}</p>
+
+                <TelegramAuth class="u-mt-10 u-mb-10"/>
+
+                <component
+                    :is="$store.getters['telegram/isAuthorized'] ? 'nuxt-link' : 'div'"
+                    class="button button--main button--full"
+                    :class="{'is-disabled': !$store.getters['telegram/isAuthorized']}"
+                    :to="$i18nGetPreferredPath('/onboarding/battle/create-portfolio')"
+                >
+                    {{ $td('Create portfolio', 'portfolio.create-new-link') }}
+                </component>
+            </template>
+
+            <template v-else>
+                <p>{{ $td('You’ve registered for the tournament. You already have a portfolio.', 'battle.begin-description-already-created') }}</p>
+
+                <nuxt-link
+                    class="button button--main button--full u-mt-10"
+                    :to="$i18nGetPreferredPath(`/portfolio/${portfolio.id}`)"
+                >
+                    {{ $td('View portfolio', 'portfolio.view-button') }}
+                </nuxt-link>
+            </template>
         </div>
     </div>
 </template>
