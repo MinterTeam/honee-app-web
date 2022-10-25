1556a92598aaae53407aba3baaaff372484de7a7
shrpne <shrpne@gmail.com>
2022-10-25 15:22:21 +0300

    fix(battle): make links inside embed open auth page

--- pages/embed/battle.vue
+++ pages/embed/battle.vue
@@ -6,9 +6,29 @@ export default {
     components: {
         PortfolioBattleTable,
     },
+    mounted() {
+        this.$refs.embedBattleTable.$el.addEventListener('click', this.handleClick, true);
+    },
+    beforeDestroy() {
+        this.$refs.embedBattleTable.$el.removeEventListener('click', this.handleClick, true);
+    },
+    methods: {
+        /**
+         *
+         * @param {Event} event
+         */
+        handleClick(event) {
+            if (event.target.nodeName === 'A') {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                const url = window.location.origin + this.$i18nGetPreferredPath('/auth/battle');
+                window.open(url, '_blank');
+            }
+        },
+    },
 };
 </script>
 
 <template>
-    <PortfolioBattleTable :limit="10"/>
+    <PortfolioBattleTable ref="embedBattleTable" :limit="10"/>
 </template>
