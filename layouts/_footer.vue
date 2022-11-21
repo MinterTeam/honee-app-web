9d69f001c9d749728b9b9da3d43efd05f29ae614
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2022-11-21 13:27:25 +0300

    update disclaimer

--- layouts/_footer.vue
+++ layouts/_footer.vue
@@ -9,11 +9,10 @@ export default {
         <div class="u-container u-container--large">
             <div class="footer__disclaimer">
                 <h3 class="u-h3 u-mb-05">{{ $td('Disclaimer', 'index.footer-disclaimer-title') }}</h3>
-                <p>{{ $td('Please note that you are accessing the ALPHA Version of the Honee service which is in the process of being tested before its official release. The sole purpose of this ALPHA Version is to conduct testing and obtain feedback.', 'index.footer-disclaimer-p1') }}</p>
-                <p>{{ $td('ALPHA testing is free for users and we do not demand any remuneration for access to the ALPHA Version of the services. Intra-blockchain fees you pay for transactions are not the payment for using the Honee service.', 'index.footer-disclaimer-p2') }}</p>
-                <p>{{ $td('Operations with virtual assets pose intrinsic risks, including the risk of monetary loss. We strongly suggest you seek professional advice before engaging in any operations with virtual assets.', 'index.footer-disclaimer-p3') }}</p>
-                <p>{{ $td('We do not give any warranties, whether express or implied, as to the suitability or usability of the service, its software or any of its content. We will not be liable for any loss, whether such loss is direct, indirect, special or consequential, suffered by any party as a result of their use of the ALPHA, its content and functionalities.', 'index.footer-disclaimer-p4') }}</p>
-                <p>{{ $td('Should you encounter any bugs, glitches, lack of functionality or other problems on the ALPHA, please email us at', 'index.footer-disclaimer-p5') }} <a href="mailto:hello@honee.app" class="link--default-black">hello@honee.app</a>.</p>
+                <p>{{ $td('Operations with virtual assets pose intrinsic risks, including the risk of monetary loss. We strongly suggest you seek professional advice before engaging in any operations with virtual assets.', 'index.footer-disclaimer-p1') }}</p>
+                <p>{{ $td('We do not give any warranties, whether express or implied, as to the suitability or usability of the service, its software or any of its content. We will not be liable for any loss, whether such loss is direct, indirect, special or consequential, suffered by any party as a result of their use of the Honee service, its content and functionalities.', 'index.footer-disclaimer-p2') }}</p>
+                <p>{{ $td('Intra-blockchain fees you pay for transactions are not the payment for using the Honee service.', 'index.footer-disclaimer-p3') }}</p>
+                <p>{{ $td('Should you encounter any bugs, glitches, lack of functionality or other problems on the Honee service, please email us at', 'index.footer-disclaimer-p4') }} <a href="mailto:hello@honee.app" class="link--default-black">hello@honee.app</a>.</p>
             </div>
             <div class="footer__menu u-grid u-grid--medium u-grid--vertical-margin--medium">
                 <div class="u-cell u-cell--auto">
