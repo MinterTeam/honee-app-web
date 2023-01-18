d0482f877d2f914a04aaaf58f1d787ca1b49ceee
shrpne <shrpne@gmail.com>
2023-01-18 17:11:21 +0300

    fix(smart-wallet): remove RFQ orders from liquidity sources

--- api/swap-hub-deposit-proxy.js
+++ api/swap-hub-deposit-proxy.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import {Cache, cacheAdapterEnhancer} from 'axios-extensions';
 import {HUB_DEPOSIT_PROXY_API_URL, HUB_DEPOSIT_PROXY_CONTRACT_ADDRESS, NATIVE_COIN_ADDRESS, HUB_CHAIN_BY_ID} from "~/assets/variables.js";
 import addToCamelInterceptor from '~/assets/axios-to-camel.js';
+import {prepareProtocolsCached} from '~/api/swap-1inch.js';
 
-//@TODO exclude limit orders https://api.1inch.io/v5.0/56/liquidity-sources
 const instance = axios.create({
     baseURL: HUB_DEPOSIT_PROXY_API_URL,
     adapter: cacheAdapterEnhancer(axios.defaults.adapter, { enabledByDefault: false}),
@@ -18,7 +18,12 @@ const fastCache = new Cache({ttl: 2 * 1000, max: 100});
  * @param {OneInchExchangeControllerGetSwapParams&{destination: string, refundTo?: string}} swapParams
  * @return {Promise<{toTokenAmount: string, txList: Array<OneInchTx>, steps: object}>}
  */
-export function buildTxForSwap(chainId, swapParams) {
+export async function buildTxForSwap(chainId, swapParams) {
+    const protocols = await prepareProtocolsCached(chainId);
+    swapParams = {
+        ...swapParams,
+        protocols,
+    };
     return instance.get(`new/swap`, {
         params: {
             // destReceiver: HUB_DEPOSIT_PROXY_CONTRACT_ADDRESS,
