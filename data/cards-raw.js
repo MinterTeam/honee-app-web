1e4fc9db0b615aab6bbe3af453698f093d41c489
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2023-02-02 10:02:48 +0300

    Change Metagarden card style

--- data/cards-raw.js
+++ data/cards-raw.js
@@ -8,7 +8,7 @@ export default {
         title: 'Earning Options',
         cards: [
             {
-                style: 'metagarden',
+                style: 'garden',
                 caption: 'Mining',
                 title: 'Buy mining spots',
                 coin: `METAGARDEN`,
