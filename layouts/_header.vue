2deefac87b0f713a6a36a0b26351c957bca68cb8
shrpne <shrpne@gmail.com>
2023-02-03 23:24:01 +0300

    refactor(header): remove duplication

--- layouts/_header.vue
+++ layouts/_header.vue
@@ -69,41 +69,33 @@ export default {
                 </nuxt-link>
             </div>
 
-            <div class="header__controls">
-                <template v-if="!isPremiumPage && isAuthorized && !simple">
-                    <nuxt-link class="header__controls-link u-flex u-flex--align-center u-hidden-large-down" :to="$i18nGetPreferredPath('/premium')">
-                        <img class="u-mr-05" src="/img/icon-premium-fancy.svg" alt="" role="presentation" width="64" height="42">
-                        {{ $t('premium.activate-title-short') }}
-                    </nuxt-link>
-                    <hr class="header__controls-link header__controls-divider u-hidden-large-down"/>
-                    <ReferralCard class="u-flex" button-class="header__controls-link u-hidden-large-down u-semantic-button"/>
-                    <hr class="header__controls-link header__controls-divider u-hidden-large-down"/>
-                </template>
-
-                <nuxt-link v-if="isAuthorized && !simple" :to="$i18nGetPreferredPath('/receive')" class="header__controls-link header__controls-user">
-                    <div class="header__controls-user-avatar u-hidden-mini-down" :style="`background-image: url(${$store.getters.avatar});`" v-if="$store.getters.avatar"></div>
-                    <div class="header__controls-user-name">{{ $store.getters.username }}</div>
-                </nuxt-link>
-                <button v-if="isAuthorized && !simple" type="button" class="header__controls-link link u-semantic-button" @click="logout()">
-                    <img src="/img/icon-logout.svg" width="24" height="24" alt="Logout">
-                </button>
-                <nuxt-link v-if="!isAuthorized && !simple && !isAuthPage" :to="$i18nGetPreferredPath('/auth')" type="button" class="header__controls-link">
-                    {{ $td('Sign in', 'index.sign-in') }}
-                </nuxt-link>
-                <div class="header__controls-language header__controls-link">
-                    <Language/>
-                </div>
-            </div>
-        </div>
-        <div class="header__container u-container u-container--wide u-hidden-large-up u-flex--justify-center" v-if="!isPremiumPage && isAuthorized && !simple">
-            <div class="header__controls">
-                <nuxt-link class="header__controls-link u-flex u-flex--align-center" :to="$i18nGetPreferredPath('/premium')">
-                    <img class="u-mr-05" src="/img/icon-premium.svg" alt="" role="presentation" width="24" height="24">
+            <!--<div class="header__controls">-->
+            <template v-if="!isPremiumPage && isAuthorized && !simple">
+                <hr class="header__horizontal-divider header__premium-item u-hidden-large-up"/>
+                <nuxt-link class="header__controls-link u-flex u-flex--align-center header__premium-item" :to="$i18nGetPreferredPath('/premium')">
+                    <img class="u-mr-05 u-hidden-large-down" src="/img/icon-premium-fancy.svg" alt="" role="presentation" width="64" height="42">
+                    <img class="u-mr-05 u-hidden-large-up" src="/img/icon-premium.svg" alt="" role="presentation" width="24" height="24">
                     {{ $t('premium.activate-title-short') }}
                 </nuxt-link>
-                <hr class="header__controls-link header__controls-divider"/>
-                <ReferralCard class="u-flex" button-class="header__controls-link u-hidden-large-up u-semantic-button"/>
+                <hr class="header__controls-link header__controls-divider header__premium-item"/>
+                <ReferralCard class="u-flex header__premium-item" button-class="header__controls-link u-semantic-button"/>
+                <hr class="header__controls-link header__controls-divider header__premium-item u-hidden-large-down"/>
+            </template>
+
+            <nuxt-link v-if="isAuthorized && !simple" :to="$i18nGetPreferredPath('/receive')" class="header__controls-link header__controls-user">
+                <div class="header__controls-user-avatar u-hidden-mini-down" :style="`background-image: url(${$store.getters.avatar});`" v-if="$store.getters.avatar"></div>
+                <div class="header__controls-user-name">{{ $store.getters.username }}</div>
+            </nuxt-link>
+            <button v-if="isAuthorized && !simple" type="button" class="header__controls-link link u-semantic-button" @click="logout()">
+                <img src="/img/icon-logout.svg" width="24" height="24" alt="Logout">
+            </button>
+            <nuxt-link v-if="!isAuthorized && !simple && !isAuthPage" :to="$i18nGetPreferredPath('/auth')" type="button" class="header__controls-link">
+                {{ $td('Sign in', 'index.sign-in') }}
+            </nuxt-link>
+            <div class="header__controls-language header__controls-link">
+                <Language/>
             </div>
+            <!--</div>-->
         </div>
     </header>
 </template>
