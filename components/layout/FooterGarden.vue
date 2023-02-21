33b1af3fd2b393e126b9157bdba1b73b07500ad4
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2023-02-21 21:51:35 +0300

    Metagarden account button

--- components/layout/FooterGarden.vue
+++ components/layout/FooterGarden.vue
@@ -9,11 +9,11 @@ export default {
         <div class="u-container u-container--large">
             <template v-if="$i18n.locale === 'en'">
                 <p><strong>Metagarden</strong> — a play-to-earn platform with mini games.</p>
-                <a href="https://t.me/metagarden" target="_blank" class="button button--medium button--garden u-mt-10">Join the community</a>
+                <a href="/metagarden/account" class="button button--medium button--garden u-mt-10">Metagarden account</a>
             </template>
             <template v-if="$i18n.locale === 'ru'">
                 <p><strong>Metagarden</strong> — play-to-earn платформа с мини-играми.</p>
-                <a href="https://t.me/metagardenru" target="_blank" class="button button--medium button--garden u-mt-10">Присоединиться к сообществу</a>
+                <a href="/ru/metagarden/account" class="button button--medium button--garden u-mt-10">Аккаунт Metagarden</a>
             </template>
         </div>
     </div>
