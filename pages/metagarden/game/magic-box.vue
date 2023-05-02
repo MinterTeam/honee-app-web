6f7352ae2a896e81f66ab07c5d39a100c8d85907
2z2z2z <61245073+2z2z2z@users.noreply.github.com>
2023-05-02 09:31:47 +0300

    wonder fix

--- pages/metagarden/game/magic-box.vue
+++ pages/metagarden/game/magic-box.vue
@@ -1,12 +1,10 @@
 <script>
 import MetagardenGame from '~/components/MetagardenGame.vue';
-import MetagardenLootboxCard from '~/components/MetagardenLootboxCard.vue';
 
 export default {
     layout: 'metagarden',
     components: {
         MetagardenGame,
-        MetagardenLootboxCard,
     },
     data() {
         return {
