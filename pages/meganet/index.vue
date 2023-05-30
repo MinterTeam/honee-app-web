caa12bfca3cf88c85a293fb919c858cea85194dd
shrpne <shrpne@gmail.com>
2023-05-31 00:51:16 +0200

    feat(megachain): add meganet progress

--- pages/meganet/index.vue
+++ pages/meganet/index.vue
@@ -1,5 +1,6 @@
 <script>
 import FieldAddressDisplay from '~/components/base/FieldAddressDisplay.vue';
+import {prettyRound} from '~/assets/utils';
 
 export default {
     components: {
@@ -26,6 +27,7 @@ export default {
     computed: {
     },
     methods: {
+        prettyRound,
     },
 };
 </script>
@@ -36,11 +38,15 @@ export default {
             <h2 class="u-h3 u-mb-05">{{ $td('Become early adopter', 'meganet.early-adopter-title') }}</h2>
             <p class="u-text-medium">{{ $td('Buy MEGANET tokens to join the Metagarden Chain. All MEGANET token holders will receive native Metagarden Chain coins with a 100% bonus.', 'meganet.early-adopter-description') }}</p>
 
+
+
+            <div class="meganet__range u-mt-15" :style="`--val: 305; --min: 0; --max: 500; --step: 1`"></div>
+
+            <div class="u-mt-10 u-text-medium u-fw-700">{{ prettyRound(500000 - 305000) }} of 500 000 tokens left</div>
+
             <nuxt-link class="button button--main button--full u-mt-10" :to="$i18nGetPreferredPath('/meganet/balance')">
                 {{ $td('Buy MEGANET tokens', 'meganet.buy-button') }}
             </nuxt-link>
-
-
         </div>
 
         <div class="card card__content card__content--medium u-text-center u-mb-10">
@@ -64,3 +70,36 @@ export default {
         </div>
     </div>
 </template>
+
+<style lang="less">
+@import '~/assets/less/include/variables.less';
+
+.meganet-range-track() {
+    border-radius: 8px;
+    height: 16px;
+}
+.meganet__range {
+    .meganet-range-track(); background: #1c082f; position: relative;
+    --meganet-width-percent: var(--val) / var(--max) * 100%;
+    // progress
+    &::before {
+        .meganet-range-track();
+        content: ''; position: absolute; left: 0; top: 0;
+        background: var(--color-main);
+        width: ~"calc(max(var(--meganet-width-percent), 8px))";
+    }
+    // thumb
+    &::after {
+        content: '';
+        position: absolute;
+        width: 40px;
+        height: 40px;
+        left: ~"calc(max(var(--meganet-width-percent) - 24px, -16px))";
+        top: (-1 * (40px - 16px) / 2);
+        background: var(--color-main);
+        opacity: 0.56;
+        filter: blur(12px);
+        border-radius: 50%;
+    }
+}
+</style>
